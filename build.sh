88c43788e87458807988ca972921a41ac3976a5e
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2023-03-05 12:01:23 +0100

    Fix first invocation

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cp build-site.el build/build-site.el # I couldn't get the link to work and did not want to put the script in a subdir
+mkdir build && cp build-site.el build/build-site.el # I couldn't get the link to work and did not want to put the script in a subdir
 emacs -Q --script build-site.el
